9edca2e3b57436842ecc74f739a1f9736764980c
root <root@DESKTOP-EDC23FI.>
2025-11-20 22:55:48 +0300

    add: Dockerfile & ecosystem conf to backend repo

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,28 @@
+const {
+  PORT,
+  POSTGRES_DB,
+  POSTGRES_HOST,
+  POSTGRES_PORT,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_PGDATA,
+} = process.env;
+
+export const apps = [
+  {
+    name: 'backend',
+    script: 'dist/main.js',
+    autorestart: true,
+    env: {
+      NODE_ENV: 'production',
+      PORT: PORT || 3000,
+      POSTGRES_HOST: POSTGRES_HOST,
+      POSTGRES_PORT: POSTGRES_PORT,
+      POSTGRES_USER: POSTGRES_USER,
+      POSTGRES_PASSWORD: POSTGRES_PASSWORD,
+      POSTGRES_DB: POSTGRES_DB,
+      POSTGRES_PGDATA: POSTGRES_PGDATA,
+      JWT_SECRET: JWT_SECRET,
+    },
+  },
+];
